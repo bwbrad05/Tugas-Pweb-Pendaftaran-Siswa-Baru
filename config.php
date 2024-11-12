67e03d082ac1d8382f8df284d5cbe33269aafb1e
bwbrad05 <142643068+bwbrad05@users.noreply.github.com>
2024-11-12 14:35:27 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php
 
-$server = "localhost";
-$user = "root";
-$password = "";
-$nama_database = "daftar-siswa";
+$server = "sql211.infinityfree.com";
+$user = "if0_37693410";
+$password = "xUMJ0PN8bWmK";
+$nama_database = "if0_37693410_daftar_siswa";
 
 $db = mysqli_connect($server, $user, $password, $nama_database);
 
